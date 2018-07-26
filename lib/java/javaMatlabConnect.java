12e51b0defa1fd54e8a9a00e271ce8427705428a
Marco Pasquali <marco.pasquali@gemrad.com>
2018-07-26 08:35:17 +0200

    Busy-signal integration and more work towards getting a more Matlab-like experience in running scripts.

--- lib/java/javaMatlabConnect.java
+++ lib/java/javaMatlabConnect.java
@@ -3,15 +3,37 @@ import java.io.*;
 
 public class javaMatlabConnect {
     public static void main(String[] args) throws Exception {
+        // INPUT:
+        // args[0] = Command to be executed in Matlab
+        // args[1] = Call type: 0 - Run file
+        //                      1 - Run section, line or text
         String myEngine = "AtomMatlabEngine";
         StringWriter writer = new StringWriter();
         MatlabEngine eng = MatlabEngine.connectMatlab(myEngine);
-        File mainFile = new File(javaMatlabConnect.class.getResource("javaMatlabConnect.class").getPath());
-        String javaDir = mainFile.getParent();
+        String command = args[0];
+        int type = Integer.parseInt(args[1]);
+        String outString = "";
 
-  		eng.eval(args[0], writer, null);
-        String outString = writer.toString();
-        eng.feval(0, "printTextFromOutside", "\n" + outString);
+        if (type == 0) {
+            // Run File: 'command' is only the file path
+            File file = new File(command);
+            String fileName = file.getName().toString().split("[.]")[0];
+            eng.feval(0, "printTextFromOutside", fileName + "\n", false);
+
+            eng.eval("run(\'" + command + "\')", writer, null);
+            outString = writer.toString();
+
+            eng.feval(0, "printTextFromOutside", outString, true);
+        } else if (type == 1) {
+            eng.eval(command, writer, null);
+            outString = writer.toString();
+
+            eng.feval(0, "printTextFromOutside", "\n" + outString);
+        } else {
+            eng.eval(command, writer, null);
+            outString = writer.toString();
+        }
+
         System.out.println(outString);
 
         writer.close();
--- lib/java/printTextFromOutside.m
+++ lib/java/printTextFromOutside.m
@@ -1,25 +1,35 @@
-function printTextFromOutside(text)
-    jDesktop = com.mathworks.mde.desk.MLDesktop.getInstance;
+function printTextFromOutside(text, doAppendPrompt)
+    %% Get GUI components
     try
-        cmdWin = jDesktop.getClient('Command Window');
-        jTextArea = cmdWin.getComponent(0).getViewport.getComponent(0);
+        jTextArea = com.mathworks.mde.cmdwin.XCmdWndView.getInstance;
     catch
+        jDesktop = com.mathworks.mde.desk.MLDesktop.getInstance;
         commandwindow;
         jTextArea = jDesktop.getMainFrame.getFocusOwner;
     end
     cmdWinDoc = jTextArea.getDocument();
 
+    %% Some Reflection magic to get some usefull private methods
     cmdWinDocClass = cmdWinDoc.getClass();
-    mAppendPrompt = javaMethodEDT('getDeclaredMethod', cmdWinDocClass, 'appendPrompt', []);
-    inputClasses = javaArray('java.lang.Class',1);
+
+    % CmdWinDocument.appendprompt: append a prompt to the Command Window's text
+    if doAppendPrompt
+        mAppendPrompt = javaMethodEDT('getDeclaredMethod', cmdWinDocClass, 'appendPrompt', []);
+        mAppendPrompt.setAccessible(true);
+    end
+
+    % CmdWinDocument.shouldSyntaxHighlight: toggle on/off the Command Window's syntax highlighting
+    inputClasses = javaArray('java.lang.Class', 1);
     inputClasses(1) = java.lang.Boolean.TYPE;
     mShouldSyntaxHighlight = javaMethodEDT('getDeclaredMethod', cmdWinDocClass, 'shouldSyntaxHighlight', inputClasses);
-    mAppendPrompt.setAccessible(true);
     mShouldSyntaxHighlight.setAccessible(true);
 
+    %% Display the text
     javaMethodEDT('invoke', mShouldSyntaxHighlight, cmdWinDoc, false);
-    javaMethodEDT('append', jTextArea, sprintf(text + '\n'))
-    javaMethodEDT('invoke', mAppendPrompt, cmdWinDoc, []);
+    javaMethodEDT('append', jTextArea, sprintf(text))
+    if doAppendPrompt
+        javaMethodEDT('invoke', mAppendPrompt, cmdWinDoc, []);
+    end
     javaMethodEDT('invoke', mShouldSyntaxHighlight, cmdWinDoc, true);
     javaMethodEDT('repaint', jTextArea)
 end
